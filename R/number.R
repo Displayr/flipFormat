542c18e0a95539948eb9f8433f69857a098197b3
TimBock <tim.bock@numbers.net.au>
2016-05-31 11:50:29 +1000

    Adding commas, reducing signatures, adding tests
    FormatAsPercent, FormatAsReal

--- R/number.R
+++ R/number.R
@@ -1,29 +1,27 @@
-#' Converts a proportion to a percent and
-#' formats it nicely and with a percentage sign.
+#' Converts a proportion to a percent
 #'
 #' @param x The number(s)
-#' @param digits Number of significant digits
-#' @param format See \code{\link{formatC}}.
-#' @param suffix The text to appear after a percent. Defaults to \%.
-#' @param ... Additional arguments that are passed to \code{\link{formatC}}.
+#' @param digits Number of significant digits.
+#' @details Multiplies by 100, keeping \code{digits} or more significant digits and
+#' putting a % at the end, and commas if in thousands or more.
+#' Based on \code{\link{formatC}}.
 
 #' @export
-FormatAsPercent <- function(x, digits = 2, format = "fg", suffix = "%",  ...)
+FormatAsPercent <- function(x, digits = 2)
 {
-    paste0(formatC(100 * x, format = format, digits = digits, ...), suffix)
+    result <- paste0(formatC(100 * x, digits = digits, format = "fg",  big.mark=','), "%")
+    sub("^\\s+", "", result) #trimming whitespace
 }
 
-#' Formats real numbers nicely, as if they are to be presented as an average.
+#' Formats real numbers nicely.
 #'
 #' @param x The number(s)
 #' @param digits Number of significant digits
-#' @param format See \code{\link{formatC}}.
-#' @param ... Additional arguments that are passed to \code{\link{formatC}}.
-
+#' @details Keeping \code{digits} or more significant digits and and commas if in thousands or more.
 #' @export
-FormatAsReal <- function(x, digits = 2, format = "fg", ...)
+FormatAsReal <- function(x, digits = 2)
 {
-    formatC(x, digits = digits, format = format ,...)
+    formatC(x, digits = digits, format = "fg", big.mark=',')
 }
 #' \code{FormatAsPValue}
 #' Formats p relatively nicely, ensuring that values greater than 0.05
@@ -32,26 +30,25 @@ FormatAsReal <- function(x, digits = 2, format = "fg", ...)
 #'
 #' @param p The number(s)
 #' @param p.cutoff TODO
-#' @param ... Additional arguments that are passed to \code{\link{formatC}}.
 
 #' @export
-FormatAsPValue <- function(p, p.cutoff = 0.05, ...)
+FormatAsPValue <- function(p, p.cutoff = 0.05)
 {
     n.digits <- 2
     if (p < 0)
         return("0")
-    p.formatted <- formatC(p, digits = n.digits, format = "f" ,...)
+    p.formatted <- formatC(p, digits = n.digits, format = "f")
     # Making sure values greater than 0.05 are not shown as 0.05 due to rounding.
     while(as.numeric(p.formatted) == p.cutoff)
     {
         n.digits <- n.digits + 1
-        p.formatted <- formatC(p, digits = n.digits, format = "f" ,...)
+        p.formatted <- formatC(p, digits = n.digits, format = "f")
     }
     # Making sure values greater than 0.05 are not shown as 0.05 due to rounding.
     while(as.numeric(p.formatted) == 0 & n.digits < 12)
     {
         n.digits <- n.digits + 1
-        p.formatted <- formatC(p, digits = n.digits, format = "f" ,...)
+        p.formatted <- formatC(p, digits = n.digits, format = "f")
     }
     p.formatted
 }
---
+++ tests/testthat/test-number.R
@@ -0,0 +1,22 @@
+context("number")
+
+test_that("Percents", {
+
+    expect_equal(FormatAsPercent(0.00034), "0.034%")
+    expect_equal(FormatAsPercent(0.000341546654), "0.034%")
+    expect_equal(FormatAsPercent(.341546654), "34%")
+    expect_equal(FormatAsPercent(3.41546654), "342%")
+    expect_equal(FormatAsPercent(34.1546654), "3,415%")
+    expect_equal(FormatAsPercent(34154.6654), "3,415,467%")
+})
+
+test_that("Numbers", {
+
+    expect_equal(FormatAsReal(0.00034), "0.00034")
+    expect_equal(FormatAsReal(0.000341546654), "0.00034")
+    expect_equal(FormatAsReal(.341546654), "0.34")
+    expect_equal(FormatAsReal(3.41546654), "3.4")
+    expect_equal(FormatAsReal(3415.46654), "3,415")
+    expect_equal(FormatAsReal(3415466.54), "3,415,467")
+})
+
