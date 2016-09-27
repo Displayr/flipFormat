015503b09b0895127a8a4fdd7ea75962b5f4c4b2
JustinCCYap <justinyapcc@gmail.com>
2016-09-27 17:46:54 +1000

    CE-616: visual changes to means comparisons table

--- R/meancomparisonstable.R
+++ R/meancomparisonstable.R
@@ -21,11 +21,15 @@ MeanComparisonsTable <- function(means, zs, ps, r.squared, overall.p, column.nam
 {
     # Putting all the tables into a single data.frame, as required by formattable.
     ps[zs < 0] <- -ps[zs < 0]
-    means <- as.data.frame(cbind(means, ps, rsquared = r.squared, pvalue = overall.p))
+    winsorized.zs <- zs
+    winsorized.zs[zs < -5] <- -5
+    winsorized.zs[zs > 5] <- 5
+    colnames(winsorized.zs) <- paste0(LETTERS[1:ncol(zs)], 2)
+    means <- as.data.frame(cbind(means, ps, rsquared = r.squared, pvalue = overall.p, winsorized.zs))
     column.names <- c(column.names, "R-Squared", "<i>p</i>")
     k <- length(column.names) #Number of being compared.
 
-    # Set the number of decimails
+    # Set the number of decimals
     fixedDigits <- function(x, n = 2) {
         formatC(x, digits = n, format = "f")
     }
@@ -41,83 +45,55 @@ MeanComparisonsTable <- function(means, zs, ps, r.squared, overall.p, column.nam
         }
     )
 
-    # Add tiles to R-squarest- and z- statistics.
-    .colorScale <- function(x)
-    { # Creates a color range where 0 is white
-        min.x <- min(x)
-        max.x <- max(x)
-        #lower <- gradient(c(0, min.x, max.x),"white", "orange")[2]
-        csscolor(gradient(x, "white", "orange"))
+    .colorScale <- function(x, min = -5, max = 5)
+    {
+        csscolor(gradient(c(min, max, x), "white", "orange"))[-2:-1]
     }
-    rsquaredFormatter <-formatter(
-        "span",
-        style = x ~ style(
-            display = "block",
-            padding = "0 4px", `border-radius` = "4px",
-            `background-color` = .colorScale(x)
-        ),
-        ~ fixedDigits(rsquared, 2)
-    )
+    rsquaredFormatter <- formatter(.tag = "span", style = function(x) style(
+        display = "inline-block", direction = "rtl", `border-radius` = "4px", `padding-right` = "0px",
+        `background-color` = "pink", width = percent(x / max(x))), ~ fixedDigits(rsquared, 2))
 
-    color_tile("white", "orange")
+    formatters <- list()
+    for (i in 1:k)
+    {
+        l <- LETTERS[i]
+        txt <- sprintf("~ style(color = ifelse(abs(%s1) <= 0.05 & %s1 < 0, \"red\",ifelse(abs(%s1) <= 0.05, \"blue\", NA)),
+                       display = \"block\", padding = \"0 4px\", `border-radius` = \"4px\",
+                       `background-color` = .colorScale(%s2))", l, l, l, l)
+        formatters[[l]] <- formatter("span", style = eval(parse(text = txt)),
+                                     eval(parse(text = sprintf("%s~ fixedDigits(%s, 2)", l, l))))
+    }
+    formatters[["rsquared"]] <- rsquaredFormatter
+    formatters[["pvalue"]] <- pFormatter
 
-    formatters <- list(A = formatter("span",style = ~ ifelse(abs(A1) <= 0.05 & A1 < 0, "color:red",ifelse(abs(A1) <= 0.05, "color:blue", NA)), A~ fixedDigits(A, 2)),
-                       B = formatter("span",style = ~ ifelse(abs(B1) <= 0.05 & B1 < 0, "color:red",ifelse(abs(B1) <= 0.05, "color:blue", NA)), B~ fixedDigits(B, 2)),
-                       C = formatter("span",style = ~ ifelse(abs(C1) <= 0.05 & C1 < 0, "color:red",ifelse(abs(C1) <= 0.05, "color:blue", NA)), C~ fixedDigits(C, 2)),
-                       D = formatter("span",style = ~ ifelse(abs(D1) <= 0.05 & D1 < 0, "color:red",ifelse(abs(D1) <= 0.05, "color:blue", NA)), D~ fixedDigits(D, 2)),
-                       E = formatter("span",style = ~ ifelse(abs(E1) <= 0.05 & E1 < 0, "color:red",ifelse(abs(E1) <= 0.05, "color:blue", NA)), E~ fixedDigits(E, 2)),
-                       F = formatter("span",style = ~ ifelse(abs(F1) <= 0.05 & F1 < 0, "color:red",ifelse(abs(F1) <= 0.05, "color:blue", NA)), F~ fixedDigits(F, 2)),
-                       G = formatter("span",style = ~ ifelse(abs(G1) <= 0.05 & G1 < 0, "color:red",ifelse(abs(G1) <= 0.05, "color:blue", NA)), G~ fixedDigits(G, 2)),
-                       H = formatter("span",style = ~ ifelse(abs(H1) <= 0.05 & H1 < 0, "color:red",ifelse(abs(H1) <= 0.05, "color:blue", NA)), H~ fixedDigits(H, 2)),
-                       I = formatter("span",style = ~ ifelse(abs(I1) <= 0.05 & I1 < 0, "color:red",ifelse(abs(I1) <= 0.05, "color:blue", NA)), I~ fixedDigits(I, 2)),
-                       J = formatter("span",style = ~ ifelse(abs(J1) <= 0.05 & J1 < 0, "color:red",ifelse(abs(J1) <= 0.05, "color:blue", NA)), J~ fixedDigits(J, 2)),
-                       K = formatter("span",style = ~ ifelse(abs(K1) <= 0.05 & K1 < 0, "color:red",ifelse(abs(K1) <= 0.05, "color:blue", NA)), K~ fixedDigits(K, 2)),
-                       L = formatter("span",style = ~ ifelse(abs(L1) <= 0.05 & L1 < 0, "color:red",ifelse(abs(L1) <= 0.05, "color:blue", NA)), L~ fixedDigits(L, 2)),
-                       M = formatter("span",style = ~ ifelse(abs(M1) <= 0.05 & M1 < 0, "color:red",ifelse(abs(M1) <= 0.05, "color:blue", NA)), M~ fixedDigits(M, 2)),
-                       N = formatter("span",style = ~ ifelse(abs(N1) <= 0.05 & N1 < 0, "color:red",ifelse(abs(N1) <= 0.05, "color:blue", NA)), N~ fixedDigits(N, 2)),
-                       O = formatter("span",style = ~ ifelse(abs(O1) <= 0.05 & O1 < 0, "color:red",ifelse(abs(O1) <= 0.05, "color:blue", NA)), O~ fixedDigits(O, 2)),
-                       P = formatter("span",style = ~ ifelse(abs(P1) <= 0.05 & P1 < 0, "color:red",ifelse(abs(P1) <= 0.05, "color:blue", NA)), P~ fixedDigits(P, 2)),
-                       Q = formatter("span",style = ~ ifelse(abs(Q1) <= 0.05 & Q1 < 0, "color:red",ifelse(abs(Q1) <= 0.05, "color:blue", NA)), Q~ fixedDigits(Q, 2)),
-                       R = formatter("span",style = ~ ifelse(abs(R1) <= 0.05 & R1 < 0, "color:red",ifelse(abs(R1) <= 0.05, "color:blue", NA)), R~ fixedDigits(R, 2)),
-                       S = formatter("span",style = ~ ifelse(abs(S1) <= 0.05 & S1 < 0, "color:red",ifelse(abs(S1) <= 0.05, "color:blue", NA)), S~ fixedDigits(S, 2)),
-                       T = formatter("span",style = ~ ifelse(abs(T1) <= 0.05 & T1 < 0, "color:red",ifelse(abs(T1) <= 0.05, "color:blue", NA)), T~ fixedDigits(T, 2)),
-                       U = formatter("span",style = ~ ifelse(abs(U1) <= 0.05 & U1 < 0, "color:red",ifelse(abs(U1) <= 0.05, "color:blue", NA)), U ~ fixedDigits(U, 2)),
-                       V = formatter("span",style = ~ ifelse(abs(V1) <= 0.05 & V1 < 0, "color:red",ifelse(abs(V1) <= 0.05, "color:blue", NA)), V ~ fixedDigits(V, 2)),
-                       W = formatter("span",style = ~ ifelse(abs(W1) <= 0.05 & W1 < 0, "color:red",ifelse(abs(W1) <= 0.05, "color:blue", NA)), W ~ fixedDigits(W, 2)),
-                       X = formatter("span",style = ~ ifelse(abs(X1) <= 0.05 & X1 < 0, "color:red",ifelse(abs(X1) <= 0.05, "color:blue", NA)), X ~ fixedDigits(X, 2)),
-                       Y = formatter("span",style = ~ ifelse(abs(Y1) <= 0.05 & Y1 < 0, "color:red",ifelse(abs(Y1) <= 0.05, "color:blue", NA)), Y ~ fixedDigits(Y, 2)),
-                       Z = formatter("span",style = ~ ifelse(abs(Z1) <= 0.05 & Z1 < 0, "color:red",ifelse(abs(Z1) <= 0.05, "color:blue", NA)), Z ~ fixedDigits(Z, 2)),
-                       rsquared = rsquaredFormatter,
-                       pvalue = pFormatter)
-        # Removing unrequired formatters.
-        for (i in 26:(k + 1))
-            formatters[[i]] <- NULL
-        # Removing unwanted variables (i.e., the variables that contain the p-values)
-        p.values <- rep(FALSE, k)
-        names(p.values) <- paste0(LETTERS[1:k],"1")
-        formatters <- c(formatters, as.list(p.values))
-        subtitle.format <- if (subtitle == "") NULL else tags$h5(class=".h5",
-            style=paste0("color:", subtitleColour(), "; text-align:left; margin-top:5px; margin-bottom:0"), subtitle)
-        title.format <- if (title == "") NULL else tags$h3(class=".h3",
-            style=paste0("color:", titleColour(), "; text-align:left; margin-top:0px; margin-bottom:0"),title)
-        tbl <- format_table(means,
-                        col.names = column.names,
-                        table.attr = paste('class = "table table-condensed"',
-                                            'style = "margin:0; border-bottom: 2px solid; border-top: 2px solid; font-size:90%;"'),
-                        align = rep("r",5),
-                        caption = tagList(title.format,
-                                          subtitle.format,
-                                          tags$caption(style="caption-side:bottom;font-style:italic;font-size:90%;",
-                                        footer)),
-                        formatters = formatters)
-            browsable(
-                attachDependencies(
-                tagList(
-                    HTML(tbl)),
-                list(
-                    html_dependency_jquery(),
-                    html_dependency_bootstrap("default")
-                )
+    # Removing unwanted variables (i.e., the variables that contain the p-values and z statistics)
+    p.values <- rep(FALSE, k)
+    names(p.values) <- paste0(LETTERS[1:k], "1")
+    z.stats <- rep(FALSE, k)
+    names(z.stats) <- paste0(LETTERS[1:k], "2")
+    formatters <- c(formatters, as.list(p.values), as.list(z.stats))
+    subtitle.format <- if (subtitle == "") NULL else tags$h5(class=".h5",
+        style=paste0("color:", subtitleColour(), "; text-align:left; margin-top:5px; margin-bottom:0"), subtitle)
+    title.format <- if (title == "") NULL else tags$h3(class=".h3",
+        style=paste0("color:", titleColour(), "; text-align:left; margin-top:0px; margin-bottom:0"),title)
+    tbl <- format_table(means,
+                    col.names = column.names,
+                    table.attr = paste('class = "table table-condensed"',
+                                        'style = "margin:0; border-bottom: 2px solid; border-top: 2px solid; font-size:90%;"'),
+                    align = rep("r",5),
+                    caption = tagList(title.format,
+                                      subtitle.format,
+                                      tags$caption(style="caption-side:bottom;font-style:italic;font-size:90%;",
+                                    footer)),
+                    formatters = formatters)
+    browsable(
+        attachDependencies(
+            tagList(
+                HTML(tbl)),
+            list(
+                html_dependency_jquery(),
+                html_dependency_bootstrap("default")
+            )
         )
     )
 
