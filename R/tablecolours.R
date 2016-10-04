a35e13c5c8c53101a748668c8556171b040e6689
JustinCCYap <justinyapcc@gmail.com>
2016-10-04 17:06:39 +1100

    RegressionTable was crashing when NAs were in the table

--- R/tablecolours.R
+++ R/tablecolours.R
@@ -27,6 +27,7 @@ rSquaredColour <- function()
 #' @importFrom formattable gradient csscolor
 heatmapColourScale <- function(x, max.abs)
 {
+    x[is.na(x)] <- 0
     result <- character(length(x))
     result[x > 0] <- csscolor(gradient(c(0, max.abs, pmin(x[x > 0], max.abs)), "white", positiveSignificanceColour()))[-2:-1]
     result[x < 0] <- csscolor(gradient(c(-max.abs, 0, pmax(x[x < 0], -max.abs)), negativeSignificanceColour(), "white"))[-2:-1]
