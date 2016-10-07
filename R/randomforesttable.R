a2d91e62468894216b499626627d1c30c27f3bcb
JustinCCYap <justinyapcc@gmail.com>
2016-10-07 13:20:00 +1100

    CE-677: sort random forest tables by importance

--- R/randomforesttable.R
+++ R/randomforesttable.R
@@ -2,8 +2,8 @@
 #'
 #' Creates a pretty formattable table for random forest.
 #' @param importance A matrix of importance values from the regression.
-#' @param z.statistics A matrix of z statistics of the importance values.
-#' @param p.values A matrix of p values of the importance values.
+#' @param z.statistics A vector or matrix of z statistics of the importance values.
+#' @param p.values A vector or matrix of p values of the importance values.
 #' @param title The title for the table.
 #' @param subtitle Subtitle for the table.
 #' @param footer Text to place in the footer of the table.
@@ -13,15 +13,35 @@
 RandomForestTable <- function(importance,
                               z.statistics,
                               p.values,
+                              sort.by.importance = TRUE,
                               title = "",
                               subtitle = "",
                               footer = "",
                               p.cutoff = 0.05)
 {
-    if (ncol(importance) > 2) # Classification
+    numeric.outcome <- ncol(importance) == 2
+
+    if (sort.by.importance)
+    {
+        ind <- sort(importance[, ncol(importance)], decreasing = TRUE, index.return = TRUE)$ix
+        importance <- importance[ind, ]
+        if (!numeric.outcome)
+        {
+            z.statistics <- z.statistics[ind, ]
+            p.values <- p.values[ind, ]
+        }
+        else
+        {
+            z.statistics <- z.statistics[ind]
+            p.values <- p.values[ind]
+        }
+    }
+
+    if (!numeric.outcome) # Classification
     {
         k <- ncol(importance) - 1
         column.labels <- colnames(importance)
+        column.labels[k + 1] <- paste0("Importance (", column.labels[k + 1], ")")
         colnames(importance) <- c(paste0("importance", 1:k), "gini")
         colnames(z.statistics) <- paste0("z", 1:k)
         colnames(p.values) <- paste0("p", 1:k)
@@ -38,6 +58,7 @@ RandomForestTable <- function(importance,
     else # Regression
     {
         column.labels <- colnames(importance)
+        column.labels[2] <- paste0("Importance (", column.labels[2], ")")
         colnames(importance) <- c("importance", "mse")
         coef.df <- data.frame(importance, check.names = FALSE)
         formatters <- list(importance = createBarFormatter(decimals = 3),
