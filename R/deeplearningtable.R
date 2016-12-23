9c268e3c35254872989f0825680204f7f97378e4
chschan <chschan@gmail.com>
2016-12-23 15:50:27 +1100

    Added table for deep learning

---
+++ R/deeplearningtable.R
@@ -0,0 +1,23 @@
+#' DeepLearningTable
+#'
+#' Creates a pretty formattable table for variable importance of a deep learning network.
+#' @param values A named vector of values
+#' @param column.labels Title of column
+#' @param order.values Specifies whether the values in the table are sorted
+#' @param title The title for the table.
+#' @param subtitle Subtitle for the table.
+#' @param footer Text to place in the footer of the table.
+#' @references This is based on code written by Kenton Russell.
+#' @export
+DeepLearningTable <- function(values,
+                              column.labels = "Variable Importance",
+                              order.values = TRUE,
+                              title = "", subtitle = "", footer = "")
+{
+    table.df <- data.frame(vv = values, check.names = FALSE)
+    if (order.values)
+        table.df <- table.df[order(table.df$vv, decreasing=T),,drop=FALSE]
+    formatters <- list()
+    formatters[["vv"]] <- createBarFormatter()
+    createTable(table.df, column.labels, formatters, title, subtitle, footer)
+}
