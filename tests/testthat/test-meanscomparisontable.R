4243b3e23dd89a40d46c45b3c003fbf37a95c80b
JustinCCYap <justinyapcc@gmail.com>
2017-03-07 11:31:40 +1100

    Move unit tests to flipAnalysisOfVariance

--- tests/testthat/test-meanscomparisontable.R
+++ tests/testthat/test-meanscomparisontable.R
@@ -1,18 +1,6 @@
 context("CompareMeans")
 
 test_that("Print", {
-    library(flipAnalysisOfVariance)
-    data(cola, package = "flipExampleData")
-    suppressWarnings(CompareMultipleMeans(list(cola$Q4_A, cola$Q4_B, cola$Q4_C), cola$Q3, compare = "Columns"))
-    # Weights.
-    suppressWarnings(CompareMultipleMeans(list(cola$Q4_A, cola$Q4_B, cola$Q4_C), cola$Q3, weights = runif(length(cola$Q3)), compare = "Columns"))
-
-
-    data("bank", package = "flipExampleData")
-    bank <- bank[bank$Interest < 3  & bank$Phone > 2  & bank$Phone < 5 , ]
-    attach(bank)
-    TableOfMeans(Overall, Interest, Phone, robust.se = TRUE)
-    detach(bank)
     X <- data.frame(means1 = c(NA, 2.94),
                     means2 = c(NA, 3.59),
                     p1 = c(NA, 0.0248),
