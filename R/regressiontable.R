5112a35a6a02111408f554ea9eeb8296cdc7f276
chschan <chschan@gmail.com>
2017-03-22 15:25:46 +1100

    Add CrosstabInteractionTable

--- R/regressiontable.R
+++ R/regressiontable.R
@@ -32,3 +32,43 @@ RegressionTable <- function(coefficient.table,
     )
     createTable(coef.df, c(estimate.name, se.name, statistic.name, p.name), formatters, title, subtitle, footer)
 }
+
+#' CrosstabInteractionTable
+#'
+#' Creates pretty table showing regression coefficients with a crosstab interaction.
+#' @param coef Matrix containing estimates of the coefficient.
+#' @param coef.sign Matrix of \code{-1,0,1}s indicating whether an interaction term makes a
+#'      coefficient significantly smaller (-1) or bigger (+1) than without the interaction.
+#' @param group.size Vector containing the size of each group (i.e. interaction term).
+#' @param footer Text to place in the footer of the table.
+#' @param title The title for the table.
+#' @param subtitle Subtitle for the table.
+#' @param sign.color Vector of three colors, which controls how coefficient values which are significant will be displayed.
+#' @param decimals Number of decimals reported for the regression coefficients.
+#' @importFrom formattable area
+#' @export
+CrosstabInteractionTable <- function(coef,
+                            coef.sign,
+                            group.size,
+                            footer = "",
+                            title = "",
+                            subtitle = "",
+                            sign.color = c("red", "black", "blue"),
+                            decimals = 2)
+{
+    coef.df <- data.frame(coef, check.names = FALSE)
+    coef.df <- rbind(coef.df, n = group.size)
+
+    formatters <- list()
+    for (i in 1:(ncol(coef.df)-1))
+    {
+        i.name <- colnames(coef)[i]
+        i.col <- c(sign.color[coef.sign[,i]+2], "black")
+        formatters[[i.name]] <- formatter("span", style=style(color=i.col), x ~ FormatWithDecimals(x, decimals))
+    }
+    formatters[["NET"]] <- formatter("span", x~FormatWithDecimals(x, decimals))
+    formatters[[ncol(coef.df)+1]] <- area(row=nrow(coef.df))~formatter("span", x~FormatWithDecimals(x,0))
+
+    createTable(coef.df, col.names=colnames(coef.df), formatters, title=title, subtitle=subtitle, footer=footer)
+}
+
