6cfea53f042dce3d205dab5aff2ed781e9cc547a
JustinCCYap <justinyapcc@gmail.com>
2017-05-18 15:56:27 +1000

    DS-1181

--- R/maxdifftable.R
+++ R/maxdifftable.R
@@ -1,6 +1,33 @@
 #' MaxDiffTable
 #'
 #' Creates a pretty formattable table for max-diff output.
+#' @param stats.table A matrix of the statistics to be shown in the table.
+#' @param footer Text to place in the footer of the table.
+#' @param title The title for the table.
+#' @param subtitle Subtitle for the table.
+#' @references This is based on code written by Kenton Russell.
+#' @export
+MaxDiffTable <- function(stats.table,
+                         title = "",
+                         subtitle = "",
+                         footer = "")
+{
+    colnames(stats.table) <- c("mean", "median", "lower.quartile", "upper.quartile", "min", "max")
+    stats.table <- data.frame(stats.table, check.names = FALSE)
+    formatters <- list(
+        mean = createBarFormatter(decimals = 1),
+        median = x ~ FormatWithDecimals(x, 1),
+        lower.quartile = x ~ FormatWithDecimals(x, 1),
+        upper.quartile = x ~ FormatWithDecimals(x, 1),
+        min = x ~ FormatWithDecimals(x, 1),
+        max = x ~ FormatWithDecimals(x, 1))
+    column.labels <- c("Mean (%)", "Median (%)", "Lower<br>quartile (%)", "Upper<br>quartile (%)", "Min (%)", "Max (%)")
+    createTable(100 * stats.table, column.labels, formatters, title, subtitle, footer)
+}
+
+#' MaxDiffTableClasses
+#'
+#' Creates a pretty formattable table for max-diff output.
 #' @param probabilities A matrix of the probabilities to be shown in the table.
 #' @param column.labels Labels to show in the columns.
 #' @param footer Text to place in the footer of the table.
@@ -8,7 +35,7 @@
 #' @param subtitle Subtitle for the table.
 #' @references This is based on code written by Kenton Russell.
 #' @export
-MaxDiffTable <- function(probabilities,
+MaxDiffTableClasses <- function(probabilities,
                          column.labels,
                          title = "",
                          subtitle = "",
