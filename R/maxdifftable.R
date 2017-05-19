1c7335bf797b5afdd95adb07d707365e4b417070
JustinCCYap <justinyapcc@gmail.com>
2017-05-19 10:34:06 +1000

    DS-1181: add total column

--- R/maxdifftable.R
+++ R/maxdifftable.R
@@ -43,12 +43,16 @@ MaxDiffTableClasses <- function(probabilities,
                          footer = "")
 {
     k <- ncol(probabilities)
-    colnames(probabilities) <- paste0("class", 1:k)
+    n.classes <- k - 1
+    colnames(probabilities) <- c(paste0("class", 1:n.classes), "total")
     prob.df <- data.frame(probabilities, check.names = FALSE)
     formatters <- list()
-    for (i in 1:k)
+    for (i in 1:n.classes)
         formatters[[paste0("class", i)]] <- createBarFormatter(decimals = 1,
                                                                bar.shows.magnitude = TRUE,
                                                                max.display.value = 100 * max(prob.df))
+    formatters[["total"]] <- createBarFormatter(decimals = 1,
+                                                bar.shows.magnitude = TRUE,
+                                                max.display.value = 100 * max(prob.df))
     createTable(100 * prob.df, column.labels, formatters, title, subtitle, footer)
 }
