14510c7d1afe7b7114c5eddc66109c46b3a8dd00
JustinCCYap <justinyapcc@gmail.com>
2018-03-02 15:31:38 +1100

    DS-1787: modify SampleDescription to take effective sample size

--- R/sampledescription.R
+++ R/sampledescription.R
@@ -19,11 +19,13 @@
 #' variables being imputed. Used in the description of the imputation
 #' that typically appears in a footer.
 #' @param resample Whether resampling is used whenever weights are applied.
+#' @param effective.sample.size The effective sample size when weights are applied.
 #' @return character description of sample
 #' @export
 SampleDescription <- function(n.total, n.subset, n.estimation, subset.label, weighted = TRUE,
                               weight.label = "", missing, imputation.label = NULL, m,
-                              variable.description = "", resample = FALSE)
+                              variable.description = "", resample = FALSE,
+                              effective.sample.size = NULL)
 {
     # Warning if there is less than 50% data.
     missing.data.proportion <- if (n.subset)
@@ -36,8 +38,10 @@ SampleDescription <- function(n.total, n.subset, n.estimation, subset.label, wei
     # Creating description.
     missing.data <- n.estimation < n.subset
     imputation <-  missing == "Imputation (replace missing values with estimates)" | missing == "Multiple imputation"
-    description <- BaseDescription(paste0("n = ", n.estimation," cases used in estimation"),
-                                   n.total, n.subset, n.estimation, subset.label, weighted, weight.label, resample)
+    description <- BaseDescription(paste0("n = ", n.estimation, " cases used in estimation"),
+                                   n.total, n.subset, n.estimation,
+                                   subset.label, weighted, weight.label,
+                                   resample, effective.sample.size)
     if (variable.description != "")
         variable.description <- paste0(variable.description, " ")
     if (missing.data | imputation)
@@ -68,9 +72,11 @@ SampleDescription <- function(n.total, n.subset, n.estimation, subset.label, wei
 #' @param weighted Whether sample has been weighted.
 #' @param weight.label The label of the weight.
 #' @param resample Whether resampling is used whenever weights are applied.
+#' @param effective.sample.size The effective sample size when weights are applied.
 #' @export
 BaseDescription <- function(description.of.n, n.total, n.subset, n.estimation, subset.label,
-                            weighted = TRUE, weight.label = "", resample = FALSE)
+                            weighted = TRUE, weight.label = "", resample = FALSE,
+                            effective.sample.size = NULL)
 {
     base <- if(n.estimation < n.subset) paste0(" of a total sample size of ", n.subset) else ""
     if (n.subset < n.total)
@@ -84,6 +90,11 @@ BaseDescription <- function(description.of.n, n.total, n.subset, n.estimation, s
     }
     else
         ""
-    paste0(description.of.n, base, ";", weight.text)
+    ess.text <- if (!is.null(weighted) && weighted && !is.null(effective.sample.size))
+        paste0(" effective sample size: ",
+               FormatAsReal(effective.sample.size, decimals = 2), ";")
+    else
+        ""
+    paste0(description.of.n, base, ";", weight.text, ess.text)
 }
 
--- tests/testthat/test-sampledescription.R
+++ tests/testthat/test-sampledescription.R
@@ -8,3 +8,14 @@ test_that("No error if all data filtered (n.subset is 0)",
                               m = 1, ""))
          })
 
+test_that("Effective sample size", {
+    expect_equal(SampleDescription(n.total = 302, n.subset = 200,
+         n.estimation = 100, "lab",
+         weighted = TRUE,
+         weight, weight.label = "wgt",
+         missing = "Exclude cases with missing data",
+         effective.sample.size = 127.07),
+         paste0("n = 100 cases used in estimation of a total sample size of 200 (lab); ",
+                "data has been weighted (wgt); effective sample size: 127.07; ",
+                "cases containing missing values have been excluded;"))})
+
