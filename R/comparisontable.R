d67b69a5fcb3cba4db503d74c0697555a11b8425
jakehoare <jake_hoare@hotmail.com>
2018-08-30 14:53:23 +1000

    Update columns of ComparisonTable.

--- R/comparisontable.R
+++ R/comparisontable.R
@@ -19,7 +19,7 @@ ComparisonTable <- function(values,
         table.df <- table.df[order(table.df[, 1], decreasing = TRUE), , drop = FALSE]
 
     # Data used to lookup formatting according to column label
-    columns <- c("Training accuracy", "Evaluation accuracy", "In-sample RMSE", "Out-sample RMSE",
+    columns <- c("Training accuracy", "Evaluation accuracy", "Training RMSE", "Evaluation RMSE",
                  "Training R^2", "Evaluation R^2", "BIC", "Log-likelihood", "Time taken (s)")
     decimal.places <- c(2, 2, 2, 2, 4, 4, 0, 0, 0)
     percents <- c(TRUE, TRUE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE, FALSE)
