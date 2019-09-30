c82addc9821955a033370c6daa0b3a6a11c25bd1
jrwishart <justin.r.wishart@gmail.com>
2019-09-30 18:43:41 +1000

    DS-2648 add counts to the EntityExtractionWidget output

--- R/entityextractionwidget.R
+++ R/entityextractionwidget.R
@@ -10,8 +10,8 @@
 #'   entity extraction.
 #' @seealso \code{\link[rhtmlMetro]{Box}}
 #' @export
-EntityExtractionWidget <- function(entity.percentages, variant.percentages,
-                                   title, footer)
+EntityExtractionWidget <- function(entity.percentages, variant.percentages, entity.counts,
+                                   variant.counts, title, footer)
 {
     tfile <- createTempFile()
     cata <- createCata(tfile)
@@ -23,12 +23,14 @@ EntityExtractionWidget <- function(entity.percentages, variant.percentages,
     cata("<h1>", htmlText(title), "</h1>")
 
     cata("<table class=\"entity-extraction-table\"><thead>",
-         "<th>Entity</th><th>%</th>",
+         "<th>Entity</th><th>% (n)</th>",
          "</thead><tbody>")
 
-    entity.percentages <- sort(entity.percentages, decreasing = TRUE)
-
+    entity.percentages <- sort(entity.percentages, decreasing = TRUE, index.return = TRUE)
+    entity.counts <- entity.counts[entity.percentages$ix]
+    entity.percentages <- entity.percentages$x
     n.entities <- length(entity.percentages)
+
     for (i in 1:n.entities)
     {
         entity.name <- names(entity.percentages)[i]
@@ -39,11 +41,12 @@ EntityExtractionWidget <- function(entity.percentages, variant.percentages,
         cata("<summary class=\"summary entity-summary\">",
              "<span>", htmlText(entity.name), "</span></summary>")
 
-        percentages <- sort(variant.percentages[[entity.name]], decreasing = TRUE)
+        percentages <- sort(variant.percentages[[entity.name]], decreasing = TRUE, index.return = TRUE)
+        counts <- variant.counts[[entity.name]][percentages$ix]
 
-        t <- cbind(htmlText(names(percentages)), unname(FormatAsPercent(percentages)))
+        t <- cbind(htmlText(names(percentages$x)), unname(paste0(FormatAsPercent(percentages$x), " (", counts, ")")))
 
-        colnames(t) <- c("Variants", "%")
+        colnames(t) <- c("Variants", "% (n)")
         cata(kable(t, align = c("c", "l"),
                    format = "html", escape = FALSE,
                    table.attr = "class=\"entity-variants-table\""))
@@ -51,7 +54,7 @@ EntityExtractionWidget <- function(entity.percentages, variant.percentages,
         cata("</details>")
         cata("</td><td>")
 
-        cata(FormatAsPercent(entity.percentages[i]))
+        cata(FormatAsPercent(entity.percentages[i]), " (", entity.counts[i], ")", sep = "")
 
         cata("</td></tr>")
     }
