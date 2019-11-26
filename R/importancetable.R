4f4e06b7582b58b65a3277d25b4a6f7e300ff4a1
Justin Yap <JustinCCYap@users.noreply.github.com>
2019-11-26 15:10:07 +1100

    Ds 2702 (#11)
    * Rename relativeimportancetable to importancetable
    
    * Increment version

--- R/importancetable.R
+++ R/importancetable.R
@@ -1,7 +1,7 @@
-#' @title RelativeImportanceTable
+#' @title Create formattable table of importance scores
 #'
-#' @description Creates a pretty formattable table for relative importance output.
-#' @param relative.importance The relative importance object.
+#' @description Creates a pretty formattable table for relative/Shapley importance output.
+#' @param importance The importance object.
 #' @param row.labels The row labels corresponding to the predictor variable names/labels.
 #' @param title The title for the table.
 #' @param subtitle Subtitle for the table.
@@ -9,16 +9,16 @@
 #' @param p.cutoff The alpha level used when formatting the p-value column.
 #' @references This is based on code written by Kenton Russell.
 #' @export
-RelativeImportanceTable <- function(relative.importance,
-                                    row.labels,
-                                    title = "",
-                                    subtitle = "",
-                                    footer = "",
-                                    p.cutoff = 0.05)
+ImportanceTable <- function(importance,
+                            row.labels,
+                            title = "",
+                            subtitle = "",
+                            footer = "",
+                            p.cutoff = 0.05)
 {
-    ria <- relative.importance
-    coef.df <- data.frame(ria$importance, ria$raw.importance, ria$standard.errors,
-                          ria$statistics, ria$p.values, check.names = FALSE)
+    coef.df <- data.frame(importance$importance, importance$raw.importance,
+                          importance$standard.errors, importance$statistics,
+                          importance$p.values, check.names = FALSE)
     names(coef.df) <- c("importance", "raw.importance", "std.err", "t", "p")
     row.names(coef.df) <- row.labels
 
@@ -29,8 +29,8 @@ RelativeImportanceTable <- function(relative.importance,
         t = x ~ FormatAsReal(x, decimals = 2),
         p = createPFormatter(p.cutoff)
     )
-    column.names <- c("Relative importance", "Raw score", "Standard<br>Error",
-                      paste0("<span style='font-style:italic;'>", ria$statistic.name, "</span>"),
+    column.names <- c("Importance", "Raw score", "Standard<br>Error",
+                      paste0("<span style='font-style:italic;'>", importance$statistic.name, "</span>"),
                       "<span style='font-style:italic;'>p</span>")
     createTable(coef.df, column.names, formatters, title, subtitle, footer)
 }
