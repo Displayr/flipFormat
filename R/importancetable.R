18cbb7264ca1f0b4f69a6ab0c3ae980b9dbe6d40
jrwishart <justin.wishart@displayr.com>
2020-04-30 18:54:05 +1000

    DS-2876 Improve Documentation for ImportanceTable [ci skip]

--- R/importancetable.R
+++ R/importancetable.R
@@ -1,21 +1,39 @@
 #' @title Create formattable table of importance scores
 #'
 #' @description Creates a pretty formattable table for relative/Shapley importance output.
-#' @param driver.analysis.output A list containing the
-#'     relative importance scores and bootstrap computed p-values. relevant elements to be plotted.
-#'   This covers the case of outputs of type
+#' @param driver.analysis.output A list containing the relative importance scores, output coefficients
+#'   and significance test values. The list needs to contain the following elements:
 #'   \itemize{
-#'     \item \code{"Relative Importance Analysis"} and \code{"Shapley Regression"} where a list of importance
-#'   scores is required.
-#'     \item \code{"Jaccard Coefficient"} where a list is required that includes the Jaccard Coefficients,
-#'     \item \code{"Correlation"} where a list of correlation and relative importance scores and computed p values.
+#'   \item \code{importance}: A numeric vector of relative importance values based off the \code{raw.importance.scores}
+#'   \item \code{raw.importance.scores}: A numeric vector of coefficients, these could be the output coefficients from a Shapley
+#'    Regression or Relative Importance Analysis or the Jaccard Coefficients or Correlation coefficients if the \code{output.type} (see below) is
+#'    \code{"Shapley Regression"}, \code{"Relative Importance Analysis"}, \code{"Jaccard Coefficient"} or \code{"Correlation"}
+#'    respectively.
+#'     \item \code{p.values}: The resulting p values from the statistics (see below).
+#'    }
+#'
+#'    For \code{"Shapley Regression"} and \code{"Relative Importance Analysis"}, two more list elements are required,
+#'    \itemize{
+#'    \item \code{standard.errors}: The standard errors of the estimated coefficients
+#'    \item \code{statistics}: The computed statistics
+#'    }
+#'    For \code{"Jaccard Coefficient"} or \code{"Correlation"} outputs, the following single extra elements is required instead.
+#'    \itemize{
+#'     \item \code{sample.size} The sample size count used in the calculation of significance tests for each driver.
 #'   }
 #' @param row.labels The row labels corresponding to the predictor variable names/labels.
 #' @param title The title for the table.
 #' @param subtitle Subtitle for the table.
 #' @param footer Text to place in the footer of the table.
 #' @param p.cutoff The alpha level used when formatting the p-value column.
-#' @param output.type
+#' @param output.type A character string denoting the output type
+#'   \itemize{
+#'     \item \code{"Relative Importance Analysis"}
+#'     \item \code{"Shapley Regression"}
+#'     \item \code{"Jaccard Coefficient"}
+#'     \item \code{"Correlation"}
+#'   }
+
 #' @references This is based on code written by Kenton Russell.
 #' @export
 ImportanceTable <- function(driver.analysis.output,
