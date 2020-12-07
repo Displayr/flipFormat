3e67f1b51a6b71949609ae7b929604f9b3a64722
Justin Yap <justinyapcc@gmail.com>
2020-12-07 12:42:57 +1100

    Save ChoiceModelDesign outputs to remove dependence on flipChoice

--- tests/testthat/test-createchoicemodeldesignwidget.R
+++ tests/testthat/test-createchoicemodeldesignwidget.R
@@ -1,6 +1,15 @@
 context("Experimental design")
 
-require("flipChoice")
+# The ChoiceModelDesign outputs have been saved as RDA files to avoid having
+# to call flipChoice::ChoiceModelDesign in flipFormat (timeout when loading
+# flipChoice in Travis). The R code used to generate the outputs have been
+# left as comments.
+
+findInstDirFile <- function(file)
+{
+    file.path(system.file("testdata", package = "flipFormat", mustWork = TRUE),
+              file)
+}
 
 no.prior <- structure(c("Brand", "Hershey", "Dove", "Godiva", "Lindt", "Price",
                "$0.99", "$1.49", "$1.99", "$2.49", "Cocoa strength", "70%",
@@ -20,14 +29,15 @@ has.prior <- structure(c("Brand", "Hershey", "Dove", "Godiva", "Lindt", "mean",
 
 test_that("ChoiceModelDesign print, none alternative",
 {
-    skip_if_not_installed("flipChoice")
-    cmd <- suppressWarnings(ChoiceModelDesign(design.algorithm = "Shortcut",
-                                attribute.levels = has.prior,
-                                n.questions = 6,
-                                n.versions = 3,
-                                none.alternatives = 1,
-                                alternatives.per.question = 4,
-                                seed = 1))
+    # cmd <- suppressWarnings(ChoiceModelDesign(design.algorithm = "Shortcut",
+    #                                           attribute.levels = has.prior,
+    #                                           n.questions = 6,
+    #                                           n.versions = 3,
+    #                                           none.alternatives = 1,
+    #                                           alternatives.per.question = 4,
+    #                                           seed = 1))
+    load(findInstDirFile("choice.model.design.1.rda"))
+    cmd <- choice.model.design.1
     out <- CreateChoiceModelDesignWidget(cmd)
     expect_is(out, "htmlwidget")
     expect_equal(attr(out, "ChartData"), cmd$labeled.design)
@@ -35,15 +45,16 @@ test_that("ChoiceModelDesign print, none alternative",
 
 test_that("ChoiceModelDesign print, p.p with constant attributes",
 {
-    skip_if_not_installed("flipChoice")
-    cmd <- suppressWarnings(ChoiceModelDesign(design.algorithm = "Partial profiles",
-                                attribute.levels = has.prior,
-                                n.questions = 6,
-                                n.versions = 3,
-                                none.alternatives = 0,
-                                n.constant.attributes = 1,
-                                alternatives.per.question = 2,
-                                seed = 1))
+    # cmd <- suppressWarnings(ChoiceModelDesign(design.algorithm = "Partial profiles",
+    #                                           attribute.levels = has.prior,
+    #                                           n.questions = 6,
+    #                                           n.versions = 3,
+    #                                           none.alternatives = 0,
+    #                                           n.constant.attributes = 1,
+    #                                           alternatives.per.question = 2,
+    #                                           seed = 1))
+    load(findInstDirFile("choice.model.design.2.rda"))
+    cmd <- choice.model.design.2
     out <- print(cmd)
     expect_is(out, "htmlwidget")
 
@@ -58,14 +69,15 @@ test_that("ChoiceModelDesign print, p.p with constant attributes",
 
 test_that("ChoiceModelDesign print, 1 version with prior",
 {
-    skip_if_not_installed("flipChoice")
-    cmd <- suppressWarnings(ChoiceModelDesign(design.algorithm = "Shortcut",
-                                attribute.levels = has.prior,
-                                n.questions = 20,
-                                n.versions = 1,
-                                none.alternatives = 0,
-                                alternatives.per.question = 6,
-                                seed = 1))
+    # cmd <- suppressWarnings(ChoiceModelDesign(design.algorithm = "Shortcut",
+    #                                           attribute.levels = has.prior,
+    #                                           n.questions = 20,
+    #                                           n.versions = 1,
+    #                                           none.alternatives = 0,
+    #                                           alternatives.per.question = 6,
+    #                                           seed = 1))
+    load(findInstDirFile("choice.model.design.3.rda"))
+    cmd <- choice.model.design.3
     out <- print(cmd)
     expect_is(out, "htmlwidget")
 
@@ -86,37 +98,39 @@ test_that("ChoiceModelDesign print shows zero prior correctly",
     ## this test just checks that the user can still
     ## specify a zero prior and have that included
     ## in the prior output
-    skip_if_not_installed("flipChoice")
-    has.prior <- structure(c("Brand", "Hershey", "Dove", "Godiva", "Lindt", "mean",
-                         "0", "0", "0", "0", "Price", "$0.99", "$1.49", "$1.99", "$2.49",
-                         "Cocoa strength", "70%", "Dark", "Milk", "White", "Sugar", "Standard",
-                         "50% reduced sugar", "Sugar free", "", "Origin", "USA", "Switzerland",
-                         "Belgium", "Belgian (Single origin Venezuelan Criollo beans)",
-                         "Nuts", "Almonds", "Hazelnuts", "No", "", "Ethical", "Fair trade",
-                         "BLANK", "", ""), .Dim = c(5L, 8L))
-    cmd <- suppressWarnings(ChoiceModelDesign(design.algorithm = "Partial profiles",
-                                attribute.levels = has.prior,
-                                n.questions = 10,
-                                n.versions = 5,
-                                none.alternatives = 0,
-                                alternatives.per.question = 6,
-                                seed = 1))
+    # has.prior <- structure(c("Brand", "Hershey", "Dove", "Godiva", "Lindt", "mean",
+    #                          "0", "0", "0", "0", "Price", "$0.99", "$1.49", "$1.99", "$2.49",
+    #                          "Cocoa strength", "70%", "Dark", "Milk", "White", "Sugar", "Standard",
+    #                          "50% reduced sugar", "Sugar free", "", "Origin", "USA", "Switzerland",
+    #                          "Belgium", "Belgian (Single origin Venezuelan Criollo beans)",
+    #                          "Nuts", "Almonds", "Hazelnuts", "No", "", "Ethical", "Fair trade",
+    #                          "BLANK", "", ""), .Dim = c(5L, 8L))
+    # cmd <- suppressWarnings(ChoiceModelDesign(design.algorithm = "Partial profiles",
+    #                                           attribute.levels = has.prior,
+    #                                           n.questions = 10,
+    #                                           n.versions = 5,
+    #                                           none.alternatives = 0,
+    #                                           alternatives.per.question = 6,
+    #                                           seed = 1))
+    load(findInstDirFile("choice.model.design.4.rda"))
+    cmd <- choice.model.design.4
     expect_true(all(cmd$prior == 0))
 
     ## As of DS-2311, prior is not stripped if alg isnt one of efficient, Modfed, or p.p
-    cmd <- suppressWarnings(ChoiceModelDesign(design.algorithm = "Random",
-                                attribute.levels = has.prior,
-                                n.questions = 10,
-                                n.versions = 5,
-                                none.alternatives = 0,
-                                alternatives.per.question = 6,
-                                seed = 1))
+    # cmd <- suppressWarnings(ChoiceModelDesign(design.algorithm = "Random",
+    #                                           attribute.levels = has.prior,
+    #                                           n.questions = 10,
+    #                                           n.versions = 5,
+    #                                           none.alternatives = 0,
+    #                                           alternatives.per.question = 6,
+    #                                           seed = 1))
+    load(findInstDirFile("choice.model.design.5.rda"))
+    cmd <- choice.model.design.5
     expect_true(all(cmd$prior == 0))
 })
 
 test_that("ChoiceModelDesign print prior with sd given",
 {
-    skip_if_not_installed("flipChoice")
     prior <- matrix(c(-0.8, -0.8, -0.8, -0.8, -0.8, -0.8, rep(0.4, 6)), ncol = 2)
     attr.list <- list(A1 = 1:2, A2 = 1:2, A3 = 1:2, A4 = 1:2,
                       A5 = 1:2, A6 = 1:2)
@@ -159,10 +173,12 @@ test_that("CMD print when no standard errors; DS-2881",
                      "", "", "", "", "", "", "", "", "", "", "", "",
                      "Safety 1", "Safety 2", "Safety 2"), .Dim = c(29L,
                                                                    5L))
-    expect_warning(cmd <- ChoiceModelDesign(design.algorithm = "Balanced overlap",
-                                            prohibitions = p, attribute.levels = al,
-                                            alternatives.per.question= 3,
-                                            n.questions = 6, n.versions = 10),
-                   "Standard errors cannot be calculated")
+    # expect_warning(cmd <- ChoiceModelDesign(design.algorithm = "Balanced overlap",
+    #                                         prohibitions = p, attribute.levels = al,
+    #                                         alternatives.per.question= 3,
+    #                                         n.questions = 6, n.versions = 10),
+    #                "Standard errors cannot be calculated")
+    load(findInstDirFile("choice.model.design.6.rda"))
+    cmd <- choice.model.design.6
     expect_error(print(cmd), NA)
 })
