0f2e39e4dc74e573bbba4a872302e58dcdaeb67a
Justin Yap <justinyapcc@gmail.com>
2021-04-06 14:41:53 +1000

    Add snapshot test for widget

--- tests/testthat/test-stackingwidget.R
+++ tests/testthat/test-stackingwidget.R
@@ -1,5 +1,7 @@
 context("DataSetMergingWidget")
 
+library(flipChartTests)
+
 findInstDirFile <- function(file)
 {
     file.path(system.file("testdata", package = "flipFormat", mustWork = TRUE),
@@ -9,10 +11,10 @@ findInstDirFile <- function(file)
 load(findInstDirFile("stacking.output.rda"))
 
 test_that("stacking widget", {
-    StackingWidget(stacking.output$stacked.data.set.metadata,
+    TestWidget(StackingWidget(stacking.output$stacked.data.set.metadata,
                    list(c("Last Resp", "Q3")),
                    stacking.output$omitted.variables,
                    stacking.output$omitted.stacked.variables,
                    stacking.output$common.labels,
-                   stacking.output$is.saved.to.cloud)
+                   stacking.output$is.saved.to.cloud), "stacking-widget", height = 2500)
 })
