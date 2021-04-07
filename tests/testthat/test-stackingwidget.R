ba5193e20082b0cfe5ee7eab6bfdbe9d0ee6b10c
Justin Yap <justinyapcc@gmail.com>
2021-04-07 15:11:58 +1000

    Include png package

---
+++ tests/testthat/test-stackingwidget.R
@@ -0,0 +1,22 @@
+context("StackingWidget")
+
+library(flipChartTests)
+library(png)
+
+findInstDirFile <- function(file)
+{
+    file.path(system.file("testdata", package = "flipFormat", mustWork = TRUE),
+              file)
+}
+
+load(findInstDirFile("stacking.output.rda"))
+
+test_that("stacking widget", {
+    widget <- StackingWidget(stacking.output$stacked.data.set.metadata,
+                        list(c("Last Resp", "Q3")),
+                        stacking.output$omitted.variables,
+                        stacking.output$omitted.stacked.variables,
+                        stacking.output$common.labels,
+                        stacking.output$is.saved.to.cloud)
+    expect_true(TestWidget(widget, "stacking-widget", height = 2500))
+})
