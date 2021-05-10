65fb7f1a1351eaedf2f2a1141017e60d80a138eb
Justin Yap <justinyapcc@gmail.com>
2021-05-11 09:41:45 +1000

    Fix unit test

--- tests/testthat/test-stackingwidget.R
+++ tests/testthat/test-stackingwidget.R
@@ -14,8 +14,6 @@ load(findInstDirFile("stacking.output.rda"))
 test_that("stacking widget", {
     widget <- StackingWidget(stacking.output$stacked.data.set.metadata,
                         list(c("Last Resp", "Q3")),
-                        stacking.output$omitted.variables,
-                        stacking.output$omitted.stacked.variables,
                         stacking.output$common.labels,
                         stacking.output$is.saved.to.cloud)
     expect_true(TestWidget(widget, "stacking-widget", height = 2500))
