0d8187f0437564da92e1215dfbf0343439b2821e
chschan <chschan@users.noreply.github.com>
2022-02-01 13:13:18 +1100

    VIS-599: Fix pattern matching to allow attributes

--- R/checkimagetag.R
+++ R/checkimagetag.R
@@ -3,26 +3,33 @@ checkImageTag <- function(text)
 {
     patt <- "<img src=([\"'])((?:\\\\?+.)*?)\\1>"
     mm <- regexpr(patt, text, perl = TRUE)
-    mlen <- attr(mm, "capture.length")
-    mstart <- attr(mm, "capture.start")
-    if (mm < 0 || length(mlen) < 2 || mlen[2] < 1)
+    if (mm < 0)
     {
-        warning("Table content contains an image tag with a syntax error which has been removed: ", text)
-        return("")
+        patt2 <- "<img src=([^>]+)>"
+        mm2 <- regexpr(patt2, text, perl = TRUE)
+        if (mm2 < 0)
+        {
+            warning("Table content contains an image tag with a syntax error which has been removed: ", text)
+            return("")
+        }
+        imgtag <- substr(text, mm2, mm2 + attr(mm2, "match.length") - 1)
+        imglink <- strsplit(substr(text, attr(mm2, "capture.start")[1], attr(mm2, "capture.start")[1] +
+                                       attr(mm2, "capture.length")[1] - 1), " ")[[1]][1]
+
     } else
     {
         imgtag <- substr(text, mm, mm + attr(mm, "match.length") - 1)
-        imglink <- substr(text, mstart[2], mstart[2] + mlen[2] - 1)
-        response <- try(GET(imglink), silent = TRUE)
-        if (inherits(response, "try-error") || response$status_code != 200)
-        {
-            warning("Table content contains an image tag with an invalid link which has been removed: ", text)
-            return(sub(imgtag, "", text, fixed = TRUE))
-        } else
-        {
-            # if there is no problem with the image tag then enclose
-            # inside a div to preserve alignment inside table
-            return(paste0("<div>", text, "</div>"))
-        }
+        imglink <- substr(text, attr(mm, "capture.start")[2], attr(mm, "capture.start")[2] + attr(mm, "capture.length")[2] - 1)
+    }
+    response <- try(GET(imglink), silent = TRUE)
+    if (inherits(response, "try-error") || response$status_code != 200)
+    {
+        warning("Table content contains an image tag with an invalid link which has been removed: ", text)
+        return(sub(imgtag, "", text, fixed = TRUE))
+    } else
+    {
+        # if there is no problem with the image tag then enclose
+        # inside a div to preserve alignment inside table
+        return(paste0("<div>", text, "</div>"))
     }
 }
--- tests/testthat/test-checkimagetag.R
+++ tests/testthat/test-checkimagetag.R
@@ -11,10 +11,15 @@ test_that("checkImageTag",
     expect_equal(nchar(res), 84)
 
     txt.empty <- "<img src=''>"
-    expect_warning(checkImageTag(txt.empty), "Table content contains an image tag with a syntax error")
+    expect_warning(checkImageTag(txt.empty), "Table content contains an image tag with an invalid link")
 
     txt.badlink <- "<img src='blah'>Some text"
     expect_warning(res <- checkImageTag(txt.badlink),
                    "Table content contains an image tag with an invalid link")
     expect_equal(res, "Some text")
+
+    txt.withattr <- "<a href=https://us.coca-cola.com/products/coca-cola-zero-sugar/ target='_blank'>
+         <img src=https://wiki.q-researchsoftware.com/images/c/cb/CokeZero.png width='45' height='100'></a>"
+    expect_error(res <- checkImageTag(txt.withattr), NA)
+    expect_equal(res, paste0("<div>", txt.withattr, "</div>"))
 })
